466c5af74d4d2bf8c614ebf1a64bde4d415a5c11
cooktea <623285624@qq.com>
2019-04-13 23:01:15 +0800

    校园卡丢卡界面完成，逻辑待完善

--- miniprogram/pages/service/school_card/school_card.js
+++ miniprogram/pages/service/school_card/school_card.js
@@ -1,11 +1,11 @@
 Page({
     data:{
         card_info:{},
-        currentView:"1",//"0":校园卡信息登记页面,"1":被捡到的校园卡列表页面
-        submitType:"0",//"0":捡卡提交，"1":"丢卡提交"
+        currentView:"0",//"0":校园卡信息登记页面,"1":被捡到的校园卡列表页面
+        submitType:"",//"0":捡卡提交，"1":"丢卡提交"
         imgurl:"",
     },
-    
+
     onLoad:function(){
         this.camera = wx.createCameraContext()
     },
@@ -41,9 +41,10 @@ Page({
 
     goLostCard:function(){
         this.setData({
-            submitType:"1",
-            currentView:'1',
+            submitType:'1',
+            currentView:'0',
         })
+        console.log("...")
     },
 
     error(e){
